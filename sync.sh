92946e18cca8a4fdc6af471e11962e4e036b9b77
Mistivia <i@mistivia.com>
2026-06-10 13:23:17 +0800

    update

--- sync.sh
+++ sync.sh
@@ -3,7 +3,7 @@
 make
 python3 scripts/sitemap.py
 
-sed -i 's#href="../"#href="https://mistivia.com"#g' blog/index.html
+sed -i 's#href="../"#href="//mistivia.com"#g' blog/index.html
 
 git add *
 git commit -am "update"
@@ -15,4 +15,4 @@ cp homepage/style*.css /var/ygg/web/
 rsync -avz --delete blog/ root@raye:/volume/webroot/blog/
 rsync -avz --delete homepage/ root@raye:/volume/webroot/homepage/
 rsync -avz --delete blog/ /var/ygg/web/blog/
-sed -i 's#href="https://mistivia.com"#href="http://\[200:2829:50f2:e2f1:96e1:3d6d:e107:b39f\]/"#g' /var/ygg/web/blog/index.html
+sed -i 's#href="//mistivia.com"#href="http://\[200:2829:50f2:e2f1:96e1:3d6d:e107:b39f\]/"#g' /var/ygg/web/blog/index.html
