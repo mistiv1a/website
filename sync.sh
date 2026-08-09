8ca9cabf3c026bd13cb72259f16a4d914099ba6e
Mistivia <i@mistivia.com>
2026-08-09 12:38:34 +0800

    update

--- sync.sh
+++ sync.sh
@@ -12,7 +12,7 @@ proxychains -q git push
 cp homepage/style*.css blog/
 cp homepage/style*.css /var/ygg/web/
 
-rsync -avz --delete blog/ root@raye:/volume/webroot/blog/
-rsync -avz --delete homepage/ root@raye:/volume/webroot/homepage/
+rsync -avz --delete -e "ssh -p 8765" blog/ root@raye:/volume/webroot/blog/
+rsync -avz --delete -e "ssh -p 8765" homepage/ root@raye:/volume/webroot/homepage/
 rsync -avz --delete blog/ /var/ygg/web/blog/
 sed -i 's#href="//mistivia.com"#href="http://\[200:2829:50f2:e2f1:96e1:3d6d:e107:b39f\]/"#g' /var/ygg/web/blog/index.html
